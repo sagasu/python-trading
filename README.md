8abb3db0425d19c4ee7f8d7e1e2f85f164dcdb76
Mateusz Kopij <sagasu@gmail.com>
2021-12-03 22:39:00 +0000

    work in progress

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # python-trading
+
+required python modules, sadly many of them are not on conda and you need to use pip.
+
+* [ta-lib](https://github.com/mrjbq7/ta-lib)
+* [websocket](https://websockets.readthedocs.io/en/stable/)
+* [python binance](https://github.com/sammchardy/python-binance)
+
---
+++ rsi-trading.py
@@ -0,0 +1,12 @@
+import websocket, json, pprint, talib, numpy
+import config
+from binance.client import Client
+from binance.enums import *
+
+SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = 'ETHUSD'
+TRADE_QUANTITY = 0.05
